4a9eda0a8eba09951f3b4136a91e7718532cd35f
vitamin777 <rlaalsdn8@gmail.com>
2022-08-21 16:26:02 +0900

    refactor : 커스텀 훅을 재사용하여 POST 요청 로직 개선
    -useHttp 커스텀 훅을 재사용하였음
    -useHttp 에서 isLoading,error,sendRequest 구조 분해하여 state 관리하였음
    -sendRequest 에 createTask.bind 로 createTask 의 인자 taskText 를 enterTaskHandler 에서 사용 할 수 있게바인딩해줬음
    
    -기능 정상 작동 확인 커밋

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,52 +1,68 @@
-import { useState } from 'react';
-
+import useHttp from '../hooks/use-http'
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+	const {isLoading, error, sendRequest: sendTaskRequest} = useHttp()
+	/*const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);*/
 
-  /*Firebase 에 data 저장하려는 post 요청이 전송됨
-  * -TaskForm 이 최종적으로 제출될때 트리거됨
-  * -버튼이 클릭되고 입력된 값이 검증되는 시점*/
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-d5583-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+	const createTask = (taskText, taskData) => {
+		const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+		const createdTask = {id: generatedId, text: taskText};
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+		props.onAddTask(createdTask);
+	}
 
-      const data = await response.json();
+	/*Firebase 에 data 저장하려는 post 요청이 전송됨
+	* -TaskForm 이 최종적으로 제출될때 트리거됨
+	* -버튼이 클릭되고 입력된 값이 검증되는 시점*/
+	const enterTaskHandler = async (taskText) => {
+		sendTaskRequest({
+					url: 'https://react-http-d5583-default-rtdb.firebaseio.com/tasks.json',
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: {text: taskText},
+				}, createTask.bind(null, taskText)
+				/*createTask 에있는 taskText 인자를 enterTaskHandler 에서 사용하려면 bind 해줘야한다
+				*-bind 는 Fn 를 사전에 구성할수있게해주며 호출 즉시 함수가 실행되지 않음
+				*--두번째 인자는 호출 예정인 함수가 받는 첫 번째 인자가 됨*/
+		)
+		/* setIsLoading(true);
+		 setError(null);
+		 try {
+			 const response = await fetch(
+				 'https://react-http-d5583-default-rtdb.firebaseio.com/tasks.json',
+				 {
+					 method: 'POST',
+					 body: JSON.stringify({ text: taskText }),
+					 headers: {
+						 'Content-Type': 'application/json',
+					 },
+				 }
+			 );
 
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
+			 if (!response.ok) {
+				 throw new Error('Request failed!');
+			 }
 
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+			 const data = await response.json();
 
-  return (
-    <Section>
-      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
-      {error && <p>{error}</p>}
-    </Section>
-  );
+		 } catch (err) {
+			 setError(err.message || 'Something went wrong!');
+		 }
+		 setIsLoading(false);
+ */
+	}
+
+	return (
+			<Section>
+				<TaskForm onEnterTask={enterTaskHandler} loading={isLoading}/>
+				{error && <p>{error}</p>}
+			</Section>
+	);
 };
 
 export default NewTask;
